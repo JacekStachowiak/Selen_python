78a6a3307c89e1f283bd1a22eeca28496f2b7b7c
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-27 20:20:53 +0200

    update

--- S_E_L_E_N/windows_iframe/window_focus.py
+++ S_E_L_E_N/windows_iframe/window_focus.py
@@ -3,23 +3,44 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-
-class WindowFocus():
+class SwitchToWindow():
     
     def test(self):
         
         driver = webdriver.Chrome(ChromeDriverManager().install())
+        baseUrl = 'https://courses.letskodeit.com/practice'
+        driver.get(baseUrl)
         driver.maximize_window()
-        driver.get('https://courses.letskodeit.com/practice')
         driver.implicitly_wait(4)
-
-        height = driver.execute_script("return window.innerHeight;")
-        width = driver.execute_script("return window.innerWidth;")
-        print(f'Height window is: {height}')
-        print(f'Width window is: {width}')
-       
+        
+        # find parrent handle --> Main Window
+        parentHandle = driver.current_window_handle
+        print(f'Parent handle: {parentHandle}')
+                
+        # Find open window button and click()
+        driver.find_element(By.ID, 'openwindow').click()
+        time.sleep(2)
+        
+        # find all handle
+        handles = driver.window_handles
+        
+        # switch to windows and search course
+        for handle in handles:
+            print(f' Handle: {handle}')
+            if handle not in parentHandle:
+                driver.switch_to.window(handle)
+                print(f'Switched to window: {handle}')
+                search = driver.find_element(By.ID, 'search')
+                search.send_keys('python')
+                time.sleep(3)
+                driver.close()   
+                break
+        
+        # back to parentHandle
+        driver.switch_to.window(parentHandle)
+        driver.find_element(By.ID, 'name').send_keys('Test success')
+        
         driver.quit()
 
-run_test = WindowFocus()
+run_test = SwitchToWindow()
 run_test.test()  
