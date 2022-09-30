1c5f9c479f2c1bbc30dc5b4a186c1e523062be25
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-30 21:30:21 +0200

    new file

---
+++ Testowanie/Katalog_pytest/test_case_demo3.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.fixture()       # nie muszę wpisywać yield przed fixture
+def setUp():    
+    print('Uruchamia się przed każdą method-ą')
+    yield 
+    print('Uruchamia się po każdej method-zie')
+
+def test_methodA(setUp):
+    print('Uruchamiam metodę A')
+
+def test_methodB(setUp):
+    print('Uruchamiam metodę B')
+
+# py.test -s test_case_demo2.py 
