392aafac1b9c0b4cf821e76a8c7b0ae3aa71d935
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-30 13:04:25 +0200

    update

--- Testowanie/Katalog_unittest/test_case_demo.py
+++ Testowanie/Katalog_unittest/test_case_demo.py
@@ -2,7 +2,15 @@ import unittest
 
 class TestCaseDemo(unittest.TestCase):
     
+    @ classmethod
+    def setUpClass(cls):
+        print('#*' * 30)
+        print('Uruchamiam się tylko raz przed wszystkimi testami - cls')
+        print('#*' * 30)
+        print()
+        
     def setUp(self):
+        print()
         print('Uruchamiam się przed każdym testem')
     
     def test_methodA(self):
@@ -12,7 +20,13 @@ class TestCaseDemo(unittest.TestCase):
         print('Uruchamiam metodę B')          
     
     def tearDown(self):
-        print('Uruchamiam się po wykonaniu wszystkich testów')  # czyszczenie wszystkiego co zainicjowano
+        print('Uruchamiam się po wykonaniu każdego testu')  # czyszczenie wszystkiego co zainicjowano
     
+    @ classmethod
+    def tearDownClass(cls):
+        print('#*' * 30)
+        print('Uruchamiam się tylko raz po wszystkich testach - cls')
+        print('#*' * 30)
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2)    
