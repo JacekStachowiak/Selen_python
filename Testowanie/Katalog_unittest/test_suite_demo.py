6447a714bffd418368717efb3a12c5e4c3a8400d
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-30 20:15:06 +0200

    create file

---
+++ Testowanie/Katalog_unittest/test_suite_demo.py
@@ -0,0 +1,19 @@
+# tworzymy zestaw testowy/importujemy class-y z naszych plików testowych
+import unittest
+from test_class1 import TestClass1
+from test_class2 import TestClass2
+
+# get all tests from TestClass1 and TestClass2 - tworzymy zmienne
+tc1 = unittest.TestLoader().loadTestsFromTestCase(TestClass1)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(TestClass2)
+
+# zestaw testowy łączący TestClass1/TestClass2
+smoke_test = unittest.TestSuite([tc1,tc2])
+
+unittest.TextTestRunner(verbosity=2)
+
+
+
+
+
+
