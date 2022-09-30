04573c2d7d2a48892a070a9f2acc8603f0b12b4c
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-30 21:30:06 +0200

    update

--- Testowanie/Katalog_pytest/test_case_demo1.py
+++ Testowanie/Katalog_pytest/test_case_demo1.py
@@ -1,4 +1,11 @@
 import pytest
+# uruchamiamy py.test
+# py.test -v -s
+# pliki tworzymy test_example.py lub example_test.py
+# class name  TestExample
+# test method test_example
+# http://pytest.readthedocs.io/en/reorganize-docs/new-docs/user/naming_conventions.html
+
 # dopisuję do tych metod gdzie chcę np. tylko przed metodą A
 
 @pytest.fixture
@@ -8,8 +15,6 @@ def setUp():    # nazwa dowolna
 def test_methodA(setUp):
     print('Uruchamiam metodę A')
 
-def test_methodB():
+def test_methodB(setUp):
     print('Uruchamiam metodę B')
 
-# uruchamiamy py.test
-# py.test -v -s
