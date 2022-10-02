d43168706e25e212d9f170e304e4c3c6082ced1b
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-10-02 18:28:28 +0200

    create new file

---
+++ Testowanie/Katalog_pytest/test_class_demo2.py
@@ -0,0 +1,17 @@
+import pytest
+from class_to_test import SomeClassToTest
+
+@pytest.mark.usefixtures('oneTimeSetUp','setUp')  # zamiast wpisywać do kolejnych method
+class TestClassDemo():
+    
+    @pytest.fixture(autouse=True)
+    def classSetup(self):
+        self.abc = SomeClassToTest(10)  # wstawiam value
+    
+    def test_methodA(self):
+        result = self.abc.sumNumbers(2,8)
+        assert result == 20
+        print('Running method A')
+
+    def test_methodB(self):         # nie wpisujemy do każdej method classSetup
+        print('Running method B')
