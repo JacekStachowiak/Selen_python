737978ac835cf2d46e984da722893ec58954997e
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-10-03 19:05:36 +0200

    update

--- Automatyzacja framework/pages/home/login_page.py
+++ Automatyzacja framework/pages/home/login_page.py
@@ -1,7 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
 class LoginPage():
     
     def __init__(self, driver):
