8100ddea551fa72a78c8f218ad0f7ccc98861129
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-10-03 12:55:43 +0200

    update

--- Automatyzacja framework/tests/home/login_tests.py
+++ Automatyzacja framework/tests/home/login_tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+from pages.home.login_page import LoginPage
+import unittest
 
-class LoginTest():
+class LoginTest(unittest.TestCase):
     
     def testLoginValid(self):
         
@@ -12,25 +13,14 @@ class LoginTest():
         driver.maximize_window()
         driver.implicitly_wait(3)
         driver.get(baseURL)
-
-        loginLink = driver.find_element(By.XPATH, '//a[text()="Sign In"]')
-        loginLink.click()
-        time.sleep(3)
+        lp = LoginPage(driver)
+        lp.login('test@email.com','abcabc')
+       
         
-        emailField = driver.find_element(By.ID, 'email')
-        emailField.send_keys('test@email.com')
-        
-        passwordField = driver.find_element(By.ID, 'password')
-        passwordField.send_keys('abcabc')
-        
-        loginButton = driver.find_element(By.XPATH, '//input[@class="btn btn-default btn-block btn-md dynamic-button"]')
-        loginButton.click()
-        time.sleep(5)
-        allCourse = driver.find_element(By.XPATH, '//a[text()="ALL COURSES"]')
-        allCourse.click()
-        time.sleep(3)
+        userIcon = driver.find_element(By.XPATH, '//div[@class="dropdown"]')
+        if userIcon is not None:
+            print('Login Successfull')
+        else:
+            print('Login Failed')
         
         driver.quit()
-        
-run_test = LoginTest()
-run_test.testLoginValid()         
